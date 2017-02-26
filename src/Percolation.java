3b47f35540e3983f3d853677ab9ea1ddc0bbde67
tong su <tong_su@hotmail.com>
2017-02-25 21:15:14 -0500

    coursera requires col and row to start at 1

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,73 +1,87 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import static java.util.Arrays.fill;
-import static java.util.stream.IntStream.range;
+import java.util.Arrays;
+
 
 public class Percolation {
     private final WeightedQuickUnionUF sites;
     private final int size;
     private SiteStatus[] statuses;
+    private int numberOfOpenSites;
 
     public Percolation(int n) {
+        if(n <=0) {
+            throw new IllegalArgumentException();
+        }
         size = n;
         sites = new WeightedQuickUnionUF(size * size);
         statuses = new SiteStatus[size * size];
-        fill(statuses, SiteStatus.CLOSED);
+        Arrays.fill(statuses, SiteStatus.CLOSED);
+        numberOfOpenSites = 0;
 
-    }
-
-    public int size() {
-        return size;
     }
 
     public void open(int row, int col) {
+        checkSitePosition(row, col);
         if (isOpen(row, col)) return;
 
         final int index = index(row, col);
 
         statuses[index] = SiteStatus.OPEN;
+        numberOfOpenSites++;
 
-        if (row > 0)
+        if (row > 1)
             connectIfBothSitesAreOpen(index - size, index);
-        if (row < size - 1)
+        if (row < size)
             connectIfBothSitesAreOpen(index + size, index);
-        if (col > 0)
+        if (col > 1)
             connectIfBothSitesAreOpen(index - 1, index);
-        if (col < size - 1)
+        if (col < size)
             connectIfBothSitesAreOpen(index + 1, index);
     }
 
     private void connectIfBothSitesAreOpen(int site1, int site2) {
-        if (isOpen(site1) && isOpen(site2))
+        if (statuses[site1] == SiteStatus.OPEN && statuses[site2] == SiteStatus.OPEN)
             sites.union(site1, site2);
     }
 
     public boolean isOpen(int row, int col) {
-        return isOpen(index(row, col));
+        checkSitePosition(row, col);
+        return statuses[index(row, col)] == SiteStatus.OPEN;
     }
 
-    public boolean isOpen(int i) {
-        return statuses[i] == SiteStatus.OPEN;
+    private void checkSitePosition(int row, int col) {
+        if (row <= 0 || row > size) throw new ArrayIndexOutOfBoundsException();
+        if (col <= 0 || col > size) throw new ArrayIndexOutOfBoundsException();
     }
 
     public boolean percolates() {
-        return range(0, size)
-                .anyMatch(x -> isFull(size - 1, x));
+        for (int i = 1; i <= size; i++) {
+            if (isFull(size, i)) return true;
+        }
+        return false;
     }
 
     public boolean isFull(int row, int col) {
+        checkSitePosition(row, col);
         if (!isOpen(row, col)) return false;
 
         final int pos = index(row, col);
-        return range(0, size)
-                .anyMatch(x -> isOpen(0, x) && sites.connected(x, pos));
+        for (int i = 1; i <= size; i++) {
+            if (isOpen(1, i) && sites.connected(index(1, i), pos)) return true;
+        }
+        return false;
     }
 
     private int index(int row, int col) {
-        return row * size + col;
+        return (row - 1) * size + col - 1;
     }
 
-    public static enum SiteStatus {
-        OPEN, FULL, CLOSED;
+    public int numberOfOpenSites() {
+        return numberOfOpenSites;
+    }
+
+    private enum SiteStatus {
+        OPEN, CLOSED;
     }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,61 +1,51 @@
+import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.Stopwatch;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static edu.princeton.cs.algs4.StdRandom.uniform;
-import static java.lang.String.format;
 
 public class PercolationStats {
+    private int gridSize;
     private final int runs;
     private double[] thresholdPct;
-    public static void main(String[] args) {
-        if(args.length != 2) throw new IllegalArgumentException("missing size or run number");
-
-        final int size = Integer.parseInt(args[0]);
-        final int runs = Integer.parseInt(args[1]);
-
-        final PercolationStats stats = new PercolationStats(size, runs);
-        System.out.println(format("mean:            %s", stats.mean()));
-        System.out.println(format("stddev:          %s", stats.stddev()));
-        System.out.println(format("confidenceLo:    %s", stats.confidenceLo()));
-        System.out.println(format("confidenceHi:    %s", stats.confidenceHi()));
-    }
 
     public PercolationStats(int size, int runs) {
-        if(size <= 0 || runs <= 0) throw new IllegalArgumentException("size and runs must be positive integers");
-
+        if (size <= 0 || runs <= 0) throw new IllegalArgumentException("size and runs must be positive integers");
+        this.gridSize = size;
         this.runs = runs;
         thresholdPct = new double[runs];
         Stopwatch stopwatch = new Stopwatch();
         for (int i = 0; i < runs; i++) {
-            System.out.println(format("Run # %s ......", i));
             thresholdPct[i] = openSiteOneAtATimeTillPercolation(size);
         }
         final double t = stopwatch.elapsedTime();
-        System.out.println(format("%s runs for size %s took %s seconds", runs, size, t));
-        System.out.println(format("average run time is %s seconds", t/runs));
+//        showTime(size, runs, t);
     }
 
-    public double mean(){
+    private void showTime(int size, int runs, double t) {
+        System.out.println(String.format("%s runs for size %s took %s seconds", runs, size, t));
+        System.out.println(String.format("average run time is %s seconds", t / runs));
+    }
+
+    public double mean() {
         return StdStats.mean(thresholdPct);
     }
 
-    public double stddev(){
+    public double stddev() {
         return StdStats.stddev(thresholdPct);
     }
 
     public double confidenceLo() {
-        return mean() - 1.96*stddev()/Math.sqrt(runs);
+        return mean() - 1.96 * stddev() / Math.sqrt(runs);
     }
 
     public double confidenceHi() {
-        return mean() + 1.96*stddev()/Math.sqrt(runs);
+        return mean() + 1.96 * stddev() / Math.sqrt(runs);
     }
 
-
-    private static double openSiteOneAtATimeTillPercolation(int gridSize) {
+    private double openSiteOneAtATimeTillPercolation(int gridSize) {
         final Percolation p = new Percolation(gridSize);
         final double numberOfSites = gridSize * gridSize;
 
@@ -68,14 +58,31 @@ public class PercolationStats {
             openSiteRandomly(p, closedSites);
         }
 
-        return 1 - closedSites.size()/ (numberOfSites);
+        return p.numberOfOpenSites() / (numberOfSites);
 
     }
 
-    private static void openSiteRandomly(Percolation p, List<Integer> closedSites) {
-        final int uniform = uniform(closedSites.size());
+    private void openSiteRandomly(Percolation p, List<Integer> closedSites) {
+        final int uniform = StdRandom.uniform(closedSites.size());
         final int siteToOpen = closedSites.get(uniform);
-        p.open(siteToOpen / p.size(), siteToOpen % p.size());
+        p.open(siteToOpen / gridSize + 1, siteToOpen % gridSize + 1);
         closedSites.remove(uniform);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) throw new IllegalArgumentException("missing size or run number");
+
+        final int size = Integer.parseInt(args[0]);
+        final int runs = Integer.parseInt(args[1]);
+
+        final PercolationStats stats = new PercolationStats(size, runs);
+//        showStats(stats);
+    }
+
+    private static void showStats(PercolationStats stats) {
+        System.out.println(String.format("mean:            %s", stats.mean()));
+        System.out.println(String.format("stddev:          %s", stats.stddev()));
+        System.out.println(String.format("confidenceLo:    %s", stats.confidenceLo()));
+        System.out.println(String.format("confidenceHi:    %s", stats.confidenceHi()));
     }
 }
